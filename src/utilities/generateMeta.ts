a2817af4cbf433d08d86b917fba5e65bf3162e3f
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2026-03-12 13:10:36 +0800

    chore: update page title suffix from Payload Website Template to DNG branding

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -27,8 +27,8 @@ export const generateMeta = async (args: {
   const ogImage = getImageURL(doc?.meta?.image)
 
   const title = doc?.meta?.title
-    ? doc?.meta?.title + ' | Payload Website Template'
-    : 'Payload Website Template'
+    ? doc?.meta?.title + ' | DNG 1 Account 1 System 200 Countries'
+    : 'DNG 1 Account 1 System 200 Countries'
 
   return {
     description: doc?.meta?.description,
